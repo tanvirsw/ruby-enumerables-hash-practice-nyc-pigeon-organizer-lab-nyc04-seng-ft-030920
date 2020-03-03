6eeb7efae532222b8c07b10e5e433327507f337a
Tanvir Walia <tarywalia@gmail.com>
2020-03-03 23:16:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
  data.each do |key, value|
    value.each do |new_value, names|
      names.each do |name|
-       
+
        if !new_hash[name]
          new_hash[name] = {}
        end
